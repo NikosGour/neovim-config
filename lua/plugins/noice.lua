4508f9ac92b2d4a29a5fe98e7d8f923a60f411db
NikosGour <nikosgournak@gmail.com>
2026-06-11 20:09:27 +0300

    Latest

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -13,6 +13,13 @@ return {
     opts = {
       timeout = 2000,
       fps = 144,
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
     },
   },
 }
